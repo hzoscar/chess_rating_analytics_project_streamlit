53e180ab5b3edccbccb4c14f6464993365a62960
hzoscar <hz.oscar.ist@gmail.com>
2025-01-08 22:51:50 +0100

    add countries tabs

--- pages/2_Continent Analysis.py
+++ pages/2_Continent Analysis.py
@@ -8,3 +8,8 @@ st.title("Continent Analysis :world_map:")
 st.subheader("Discover Metrics, Trends, and Insights from the World of Chess")
 
 st.sidebar.title('Filters')
+
+tab1, tab2, tab3, tab4, tab5 =st.tabs(['Africa 	:earth_africa:', 'Americas 	:earth_americas:', 'Asia 	:earth_asia:', 'Europe	:european_castle:', 'Oceania :desert_island:'])
+
+with tab1:
+    st.write('Africa')
