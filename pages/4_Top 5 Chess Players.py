780b4c356ace1c2db78be10ca1b26c119b1bacfc
hzoscar <hz.oscar.ist@gmail.com>
2025-01-17 19:03:28 +0100

    add dataframe top 5 players

--- pages/4_Top 5 Chess Players.py
+++ pages/4_Top 5 Chess Players.py
@@ -1,6 +1,7 @@
 import streamlit as st
 import warnings
 warnings.filterwarnings('ignore')
+from utils import load_data
 
 
 st.set_page_config(layout="wide")
@@ -8,3 +9,23 @@ st.set_page_config(layout="wide")
 st.title("Top 5 Chess Players 	:crown:")
 
 st.video(data = r'notebooks\download.mp4')
+
+
+query = """
+SELECT name,
+    rating,
+    fed
+FROM top_10_open_players_view topv
+LIMIT 5
+"""
+
+df = load_data(query)
+
+def callable():
+    with st.sidebar:
+        st.write("**Callback called**")
+        st.write(df.loc[st.session_state.df['selection']['rows']])
+
+x = st.dataframe(df, hide_index=True, on_select = callable, selection_mode='single-row', key= 'df')
+
+
