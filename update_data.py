bcbcb85aa47f0a45e81c15211d0a3ec7fdcb461a
hzoscar <hz.oscar.ist@gmail.com>
2025-01-24 15:27:24 +0100

    import re

--- update_data.py
+++ update_data.py
@@ -1,4 +1,5 @@
 import os
+import re
 import pandas as pd
 from sqlalchemy import create_engine
 from utils_update_data import add_column_date, check_column, check_country_code, clean_df, clean_names, load_data, move_files, refresh_materialized_view, update_montlhyupdates_table_sqlalchemy, update_players_table_sqlalchemy
